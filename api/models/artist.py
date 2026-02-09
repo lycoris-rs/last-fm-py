7d3e608390492c9448f6a94afe82c41286a000dd
encore404 <encoregit404@gmail.com>
2026-02-09 15:04:59 +0100

    Added docstrings

--- api/models/artist.py
+++ api/models/artist.py
@@ -3,6 +3,15 @@ from pydantic import BaseModel, Field, field_validator
 
 
 class ArtistImage(BaseModel):
+    """
+    Represents an image associated with an artist.
+
+    Attributes:
+        url: The URL of the image.
+        size: The size category of the image. One of "small", "medium", "large",
+              "extralarge", or "mega". Optional.
+    """
+
     url: str = Field(alias="#text")
     size: Literal["small", "medium", "large", "extralarge", "mega"] | None
 
@@ -16,6 +25,18 @@ class ArtistImage(BaseModel):
 
 
 class Artist(BaseModel):
+    """
+    Basic information about an artist.
+
+    Attributes:
+        name: The artist's name.
+        listeners: Number of listeners the artist has.
+        mbid: The MusicBrainz ID for the artist. Optional.
+        url: The URL to the artist's page.
+        streamable: Whether the artist's content is streamable. (Not sure if this is correct.)
+        image: List of associated ArtistImage objects.
+    """
+
     name: str
     listeners: int
     mbid: str | None
@@ -40,6 +61,14 @@ class Artist(BaseModel):
 
 
 class ArtistStats(BaseModel):
+    """
+    Statistical information about an artist.
+
+    Attributes:
+        listeners: Number of listeners.
+        playcount: Total play count.
+    """
+
     listeners: int
     playcount: int
 
@@ -50,42 +79,115 @@ class ArtistStats(BaseModel):
 
 
 class SimilarArtist(BaseModel):
+    """
+    Represents a similar artist to the queried artist.
+
+    Attributes:
+        name: The similar artist's name.
+        url: URL to the similar artist's page.
+        image: List of associated ArtistImage objects.
+    """
+
     name: str
     url: str
     image: list[ArtistImage]
 
 
 class SimilarArtists(BaseModel):
+    """
+    Container for a list of similar artists.
+
+    Attributes:
+        artist: List of SimilarArtist objects.
+    """
+
     artist: list[SimilarArtist]
 
 
 class ArtistTag(BaseModel):
+    """
+    Represents a tag associated with an artist.
+
+    Attributes:
+        name: Name of the tag.
+        url: URL related to the tag.
+    """
+
     name: str
     url: str
 
 
 class ArtistTags(BaseModel):
+    """
+    Container for artist tags
+
+    Attributes:
+        tag: List of ArtistTag objects.
+    """
+
     tag: list[ArtistTag]
 
 
 class ArtistBioLink(BaseModel):
+    """
+    Link information within the artist's biography.
+
+    Attributes:
+        text: The display text of the link.
+        rel: The relationship attribute of the link.
+        link: The URL of the link.
+    """
+
     text: str = Field(alias="#text")
     rel: str
     link: str = Field(alias="href")
 
 
 class ArtistBioLinks(BaseModel):
+    """
+    Container for biography links.
+
+    Attributes:
+        link: An ArtistBioLink object.
+    """
+
     link: ArtistBioLink
 
 
 class ArtistBio(BaseModel):
+    """
+    Biography details for an artist.
+
+    Attributes:
+        links: Links related to the biography.
+        published: Publication date of the biography.
+        summary: A short summary of the biography.
+        content: Full content of the biography.
+    """
+
     links: ArtistBioLinks
-    published: str | None
+    published: str
     summary: str
     content: str
 
 
 class ArtistDetail(BaseModel):
+    """
+    Detailed information about an artist.
+
+    Attributes:
+        name: The artist's name.
+        mbid: The MusicBrainz ID for the artist. Optional.
+        url: URL to the artist's page.
+        image: List of associated images.
+        streamable: Whether the artist's content is streamable. (Not sure if this is correct.)
+        ontour: Whether the artist is currently on tour.
+        stats: Statistical information.
+        similar: Similar Artists.
+        tags: Tags associated with the artist.
+        bio: biography details.
+    """
+
     name: str
     mbid: str | None
     url: str
@@ -109,16 +211,44 @@ class ArtistDetail(BaseModel):
 
 
 class ArtistMatches(BaseModel):
+    """
+    Container for artist search matches.
+
+    Attributes:
+        artist: List of Artist objects representing matched artists.
+    """
+
     artist: list[Artist]
 
 
 class ArtistSearchResults(BaseModel):
+    """
+    Container for search results of artists.
+
+    Attributes:
+        artistmatches: An ArtistMatches object.
+    """
+
     artistmatches: ArtistMatches
 
 
 class ArtistSearchResponse(BaseModel):
+    """
+    Response model for artist search API calls.
+
+    Attributes:
+        results: An ArtistSearchResults object.
+    """
+
     results: ArtistSearchResults
 
 
 class ArtistDetailResponse(BaseModel):
+    """
+    Response model for artist detail API calls.
+
+    Attributes:
+        artist: An ArtistDetail object.
+    """
+
     artist: ArtistDetail
